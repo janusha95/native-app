be9c3c84b6e78bcbc7ebf5a68cdc4206417ca1d3
janusha <janushas@dsny.com>
2022-03-21 15:38:27 -0400

    adding scrollview and flatlist

--- App.js
+++ App.js
@@ -1,20 +1,73 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TextInput,
+  ScrollView,
+  FlatList,
+} from "react-native";
 
 export default function App() {
+  const [goal, setGoal] = useState("");
+  const [addGoal, setAddGoal] = useState([]);
+
+  const handleChange = (goal) => {
+    setGoal(goal);
+  };
+
+  const addChange = () => {
+    setAddGoal([...addGoal, goal]);
+  };
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js</Text>
-      <StatusBar style="auto" />
+    <View style={{ padding: 50 }}>
+      <View style={styles.container}>
+        <TextInput
+          placeholder="Course Goals"
+          style={styles.input}
+          onChangeText={handleChange}
+          value={goal}
+        />
+        <Button title="ADD" onPress={addChange} />
+      </View>
+      <FlatList
+        data={addGoal}
+        renderItem={(itemData) => (
+          <View key={goal} style={styles.listItem}>
+            <Text>{goal}</Text>
+          </View>
+        )}
+      />
+      {/* <ScrollView>
+        {addGoal.map((goals) => (
+          <View key={goal} style={styles.listItem}>
+            <Text>{goals}</Text>
+          </View>
+        ))}
+      </ScrollView> */}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: "#fff",
+    flexDirection: "row",
+    justifyContent: "space-between",
     alignItems: "center",
-    justifyContent: "center",
+  },
+  input: {
+    width: "80%",
+    borderColor: "black",
+    borderWidth: 1,
+    padding: 10,
+  },
+  listItem: {
+    padding: 10,
+    margin: 10,
+    backgroundColor: "#ccc",
+    borderColor: "black",
+    borderWidth: 1,
   },
 });
